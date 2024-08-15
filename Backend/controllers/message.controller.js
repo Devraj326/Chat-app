8a0a6f20b37795c82142ccb126139bbb3f26a7bb
Devraj326 <dev326patil@gmail.com>
2024-08-15 10:41:14 +0530

    Deployment

--- Backend/controllers/message.controller.js
+++ Backend/controllers/message.controller.js
@@ -52,7 +52,7 @@ export const getMessages = async (req, res) => {
 	try {
 		const { id: userToChatId } = req.params;
 		const senderId = req.user._id;
-
+	
 		const conversation = await Conversation.findOne({
 			participants: { $all: [senderId, userToChatId] },
 		}).populate("messages"); // NOT REFERENCE BUT ACTUAL MESSAGES
--- Frontend/src/components/messages/MessageContainer.jsx
+++ Frontend/src/components/messages/MessageContainer.jsx
@@ -4,6 +4,7 @@ import MessageInput from './MessageInput'
 import {TiMessages} from 'react-icons/ti'
 import useConversation from '../../zustand/useConversation'
 import { useAuthContext } from '../../context/AuthContext'
+import BackgroundSelector from '../backgroundselector/BackgroundSelector'
 
 const MessageContainer = () => {
     
@@ -21,9 +22,10 @@ const MessageContainer = () => {
             <>
             {/* Header */}
 
-            <div className='bg-slate-500 px-4 py-2 mb-2'>
+            <div className='bg-slate-500 px-4 py-2 mb-2 flex justify-between'>
                 <span className='label-text'>To:</span>
                 <span className='text-gray-900 font-bold'>{selectedConversation.fullName}</span>
+                <BackgroundSelector />
             </div>
 
             <Messages />
